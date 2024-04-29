01a17524f8fef974a55001b20230e4d6122f8d91
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-04-28 22:43:48 -0300

    feat: add server and creating a initial struct

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log/slog"
+	"net"
+)
+
+const defaultListenAddr = ":5001"
+
+type Config struct {
+	ListenAddr string
+}
+
+type Server struct {
+	Config
+	ln net.Listener
+}
+
+func NewServer(cfg Config) *Server {
+	if len(cfg.ListenAddr) == 0 {
+		cfg.ListenAddr = defaultListenAddr
+	}
+	return &Server{
+		Config: cfg,
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	return s.acceptLoop()
+}
+
+func (s *Server) acceptLoop() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("Accept error", "err", err)
+			continue
+		}
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+
+}
+
+func main() {
+
+}
